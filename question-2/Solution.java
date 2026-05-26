06e55ee99c5af708ba95052c3596f3cc82ab0dfd
nxuwub <calsonl@nycstudents.net>
2026-05-26 07:47:41 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,17 @@
+public class Bottle {
+    private double capacity;
+    private double amount;
 
+    public Bottle(double cap) {
+        capacity = cap;
+        amount = cap; 
+    }
+
+    public double updateAmount(double remove) {
+        amount = amount - remove; 
+        if (amount < capacity * 0.25) {
+            amount = capacity; 
+        }
+        return amount;
+    }
+}
