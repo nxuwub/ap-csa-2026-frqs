0736521e35ab7436b641945676da9f625b52b6bb
nxuwub <calsonl@nycstudents.net>
2026-05-25 23:17:55 -0400

    Update partB.java

--- question-1/partB.java
+++ question-1/partB.java
@@ -1,1 +1,17 @@
+public String getShortenedName() {
+    String result = "";
 
+    for (int i = 0; i < username.length(); i++) {
+        char ch = username.charAt(i);
+
+        if (ch == '-') {
+            if (result.length() > 0) {
+                result = result.substring(0, result.length() - 1);
+            }
+        } else {
+            result = result + ch
+        }
+    }
+
+    return result;
+}
