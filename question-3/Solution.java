f5c5330b709d2f0852349bc0f4b1f9b5c8990bbe
nxuwub <calsonl@nycstudents.net>
2026-05-26 08:13:50 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,1 +1,17 @@
+public int moreHistoryThanMathAbsences() {
+    int count = 0;
 
+    for (CourseRecord h : historyList) {
+        String id = h.getStudentID();
+        int hA = h.getAbsences();
+
+        for (CourseRecord m : mathList) {
+            if (m.getStudentID().equals(id)) {
+                if (hA > m.getAbsences()) {
+                    count = count + 1;
+                }
+            }
+        }
+    }
+    return count;
+}
