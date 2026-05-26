f645c19c812005549d68cd0de5ab2fee853cd80a
nxuwub <calsonl@nycstudents.net>
2026-05-25 23:38:56 -0400

    Update partA.java

--- question-1/partA.java
+++ question-1/partA.java
@@ -4,12 +4,10 @@ public Account(String requestedName) {
     } else {
         int n = 1;
         String attempt = requestedName + n;
-
         while (isAvailable(attempt) == false) {
             n = n + 1; 
             attempt = requestedName + n;
         }
-
         username = attempt
     }
 }
