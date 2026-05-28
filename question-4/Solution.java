9c5af445065d222b2555f00ae9e6a3fa63b200ca
nxuwub <calsonl@nycstudents.net>
2026-05-28 09:36:28 -0400

    Update Solution.java

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,17 @@
+public int getPointsForRow(int targetRow) {
+    int sum = 0;
+    String firstColor = board[targetRow][0].getColor();
+    boolean allSame = true;
 
+    for (int c = 0; c < board[targetRow].length; c++) {
+        Space s = board[targetRow][c];
+        sum += s.getPoints();
+        if (!s.getColor().equals(firstColor)) {
+            allSame = false; 
+        }
+    }
+    if (allSame) {
+        return sum * 2;
+    }
+    return sum;
+}
